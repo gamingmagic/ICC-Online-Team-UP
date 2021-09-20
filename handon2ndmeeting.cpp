7d3c52e0c2c2deb53b47a397fc5cfbfd482ec5eb
gamingmagic <53240858+gamingmagic@users.noreply.github.com>
2021-09-20 20:55:43 +0800

    Create handon2ndmeeting.cpp

---
+++ handon2ndmeeting.cpp
@@ -0,0 +1,23 @@
+#include<iostream.h>
+#include<conio.h>
+main()
+{
+int number_of_pods, peas_per_pod, total_peas;
+clrscr();
+cout<<"Enter the number of pod:\n";
+cin>>number_of_pods;
+cout<<"Enter the number of peas in a pod:\n";
+cin>>peas_per_pod;
+total_peas = number_of_pods * peas_per_pod;
+cout<<" If you have ";
+cout<<number_of_pods;
+cout<<" peas pod \n ";
+cout<<" and ";
+cout<< peas_per_pod;
+cout<<" peas in each pod, then \n ";
+cout<<" You have ";
+cout<< total_peas;
+cout<< " peas in all the pod \n";
+getch ()
+return 0;
+}
