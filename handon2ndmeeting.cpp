74da35174a03355ebc340f156aaa4c51e821ceec
Angelrios1218 <91069055+Angelrios1218@users.noreply.github.com>
2021-09-21 17:12:38 +0800

    Update handon2ndmeeting.cpp

--- handon2ndmeeting.cpp
+++ handon2ndmeeting.cpp
@@ -9,7 +9,7 @@ cin>>numberofpods;
 cout<<"Enter the number of peas in a pod:\n";
 cin>>peasperpod;
 total_peas = numberofpods * peasperpod;
-cout<<" If you have "<<numberofpods<<"pods \n "<<" and<< peasperpod<<" peas in each pod, then \n ";
+cout<<" If you have "<<numberofpods<<"pods \n "<<" and"<< peasperpod<<" peas in each pod, then \n ";
 cout<<" You have "<<total_peas<<" peas in all pod";
 getch ();
 
