a15389f5be398adbdf9b93080055b3bd5400dc58
Angelrios1218 <91069055+Angelrios1218@users.noreply.github.com>
2021-09-21 17:11:34 +0800

    Update handon2ndmeeting.cpp

--- handon2ndmeeting.cpp
+++ handon2ndmeeting.cpp
@@ -2,19 +2,14 @@
 #include<conio.h>
 void main()
 {
-int number_of_pods, peas_per_pod, total_peas;
+int numberofpods, peasperpod, totalpeas;
 clrscr();
 cout<<"Enter the number of pod:\n";
-cin>>number_of_pods;
+cin>>numberofpods;
 cout<<"Enter the number of peas in a pod:\n";
-cin>>peas_per_pod;
-total_peas = number_of_pods * peas_per_pod;
-cout<<" If you have ";
-cout<<number_of_pods;
-cout<<" peas pod \n ";
-cout<<" and ";
-cout<< peas_per_pod;
-cout<<" peas in each pod, then \n ";
+cin>>peasperpod;
+total_peas = numberofpods * peasperpod;
+cout<<" If you have "<<numberofpods<<"pods \n "<<" and<< peasperpod<<" peas in each pod, then \n ";
 cout<<" You have "<<total_peas<<" peas in all pod";
 getch ();
 
